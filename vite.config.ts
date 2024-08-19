678ac4359c62f0c206a99380f9bae10fc9cdaa37
tmatsiievskyi1 <matsiievskyi.taras@gmail.com>
2024-08-19 16:46:05 +0300

    check

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
@@ -12,5 +13,23 @@ export default defineConfig({
     setupFiles: ['./src/setupTest.ts'],
     environment: 'jsdom',
   },
-  plugins: [react()],
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      name: '@tmatsiievskyi/react_ui_components',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
 });
